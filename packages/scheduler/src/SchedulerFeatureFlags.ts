4829b20f45b91131551738252d34dc99be0641ea
Mei Huang <mei.h.hmi@gmail.com>
2024-09-16 15:23:23 +0800

    feat: packages/react-reconciler，創建 Fiber 和 FiberRoot

--- packages/scheduler/src/SchedulerFeatureFlags.ts
+++
@@ -1,8 +0,0 @@
-export const enableSchedulerDebugging = false;
-export const enableProfiling = false;
-export const frameYieldMs = 5;
-// v8 中的最大值，Math.pow(2, 31) -1
-export const maxSigned31BitInt = 1073741823;
-export const userBlockingPriorityTimeout = 250;
-export const normalPriorityTimeout = 5000;
-export const lowPriorityTimeout = 10000;
